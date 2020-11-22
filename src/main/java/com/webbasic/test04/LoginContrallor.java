ddfc96230c7a6051899312e354914d456da3d1a2
kelvin_Wong <wyh199993@foxmail.com>
2020-11-22 23:32:28 +0800

    test04

---
+++ src/main/java/com/webbasic/test04/LoginContrallor.java
@@ -0,0 +1,12 @@
+package com.webbasic.test04;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class LoginContrallor {
+    @RequestMapping("/")
+    public String hello(Model m) {
+        m.addAttribute("name", "thymeleaf");
+        return "Login";
+    }
+}
